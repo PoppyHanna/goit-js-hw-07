729174f7716c918ad6438364cb67eaf422d5b85c
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2024-01-14 22:07:30 -0500

    update task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,22 +1,20 @@
- function handleFormSubmit(event) {
-      event.preventDefault(); 
-      const form = event.target;
-      const formData = {};
+const form = document.querySelector('.login-form');
 
-      
-      for (const input of form.elements) {
-        if (input.tagName === 'INPUT') {
-          formData[input.name] = input.value.trim();
-        }
-      }
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
-      if (Object.values(formData).some(value => value === '')) {
-        alert('All form fields must be filled in');
-        return;
-      }
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+    return;
+  }
 
-      console.log(formData);
+  const formData = {
+    email,
+    password
+  };
 
-     
-      form.reset();
-    }
+  console.log(formData);
+  form.reset();
+});

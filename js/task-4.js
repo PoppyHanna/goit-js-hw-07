374fa7affc09e68f9dfc31d7d047fc6f56e920e9
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2024-01-14 21:54:26 -0500

    create task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+ function handleFormSubmit(event) {
+      event.preventDefault(); 
+      const form = event.target;
+      const formData = {};
 
+      
+      for (const input of form.elements) {
+        if (input.tagName === 'INPUT') {
+          formData[input.name] = input.value.trim();
+        }
+      }
+
+      if (Object.values(formData).some(value => value === '')) {
+        alert('All form fields must be filled in');
+        return;
+      }
+
+      console.log(formData);
+
+     
+      form.reset();
+    }
